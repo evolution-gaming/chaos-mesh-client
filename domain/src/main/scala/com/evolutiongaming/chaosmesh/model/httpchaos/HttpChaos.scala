de28ffa4eaf0d97e3911171101aad4e7c8e876b6
Andrei Mikhailov <amikhailov@evolution.com>
2022-05-18 16:51:53 +0400

    Fixes for HttpChaos model

--- domain/src/main/scala/com/evolutiongaming/chaosmesh/model/httpchaos/HttpChaos.scala
+++ domain/src/main/scala/com/evolutiongaming/chaosmesh/model/httpchaos/HttpChaos.scala
@@ -45,6 +45,7 @@ object HttpChaos {
     * Takes effect for all methods by default
     * @param requestHeaders - Matches request headers to the target service.
     * Takes effect for all requests by default.
+    * Note: all of a sudden field should be encoded in snake case
     * @param abort - Indicates whether to inject the fault that interrupts server connection
     * @param delay - Specifies the time for a latency fault
     * @param replace - Specifies replaced values in request/response
@@ -52,14 +53,13 @@ object HttpChaos {
     * @param scheduler - Specifies the scheduling rules for the time of a specific experiment
     */
   final case class Spec(
-    mode:     Mode,
-    selector: Selectors[Selectors.Filled],
-    duration: FiniteDuration,
-    target:   HttpChaos.Target,
-    port:     Int,
-    path:     Option[String] = None,
-    method:   Option[String] = None,
-    // Note: all of a sudden field should be encoded in snake case
+    mode:           Mode,
+    selector:       Selectors[Selectors.Filled],
+    duration:       FiniteDuration,
+    target:         HttpChaos.Target,
+    port:           Int,
+    path:           Option[String] = None,
+    method:         Option[String] = None,
     requestHeaders: Option[NonEmptyMap[String, String]] = None,
     abort:          Boolean = false,
     delay:          FiniteDuration = 0.seconds,
@@ -96,9 +96,40 @@ object HttpChaos {
     def withReplaceBodyBytes(first: Byte, rest: Byte*) =
       updateReplace(_.copy(body = NonEmptyList.of(first, rest: _*).some))
 
+    /**
+      * Specifies the type of patch faults of the request body or response body.
+      *
+      */
+    def withPatchBodyType(bodyType: String) =
+      updatePatch(_.updateBody(_.copy(`type` = bodyType.some)))
+
+    /**
+      * Specifies the fault of the request body or response body with patch faults
+      *
+      */
+    def withPatchBodyContent(bodyContent: String) =
+      updatePatch(_.updateBody(_.copy(value = bodyContent.some)))
+
+    /**
+      * Specifies the attached key pair of the request headers or response headers with patch faults
+      *
+      */
+    def withPatchHeaders(first: (String, String), rest: (String, String)*) = {
+      val asNelFirst = NonEmptyList.of(first._1, first._2)
+      val asNelRest = rest.map {
+        case (first, second) => NonEmptyList.of(first, second)
+      }
+      updatePatch(_.copy(headers = NonEmptyList.of(asNelFirst, asNelRest: _*).some))
+    }
+
     private def updateReplace(f: Replace => Replace) = {
       val updatedReplace = replace.fold(Replace())(f)
       copy(replace = updatedReplace.some)
+    }
+
+    private def updatePatch(f: Patch => Patch) = {
+      val updatedPatch = patch.fold(Patch())(f)
+      copy(patch = updatedPatch.some)
     }
   }
 
@@ -106,6 +137,12 @@ object HttpChaos {
 
   object Target {
 
+    /**
+      * Specifies rules for request to target
+      *
+      * @param replace - Specifies replace rules for request to target
+      * @param patch - Specifies patch rules for request to target
+      */
     final case class Request(
       replace: Option[RequestReplace] = None,
       patch:   Option[RequestPatch] = None,
@@ -155,9 +192,18 @@ object HttpChaos {
       }
     }
 
+    /**
+      * Specifies rules for target response
+      *
+      * @param code - Specifies match by the status code responded by target.
+      * By default takes effect for all status codes
+      * @param responseHeaders - Specifies match by headers in response from target.
+      * Takes effect for all responses by default.
+      * Note: all of a sudden field should be encoded in snake case
+      * @param replace - Specifies replace rules for response by target
+      */
     final case class Response(
-      code: Option[Int],
-      // Note: all of a sudden field should be encoded in snake case
+      code:            Option[Int],
       responseHeaders: Option[NonEmptyMap[String, String]],
       replace:         Option[ResponseReplace] = None,
     ) extends Target {
@@ -170,14 +216,14 @@ object HttpChaos {
         copy(code = code.some)
 
       /**
-        * Matches request headers to target. Takes effect for all responses by default.
+        * Specifies match by headers in response from target. Takes effect for all responses by default
         *
         */
       def withMatchResponseHeaders(headers: (String, String)*) =
         copy(responseHeaders = NonEmptyMap.fromMap(SortedMap(headers: _*)))
 
       /**
-        * Specifies the replaced content of the response status code.
+        * Specifies the replaced content of the response status code
         *
         */
       def withReplacedResponseCode(code: Int) =
@@ -187,11 +233,31 @@ object HttpChaos {
         val updated = replace.fold(ResponseReplace())(f)
         copy(replace = updated.some)
       }
-
     }
-
   }
 
+  /**
+    * Specifies rules for patching request or response
+    *
+    * @param headers - Specifies the attached key pair of the request headers or response headers with patch faults
+    * @param body - Specifies request body patch
+    */
+  final case class Patch(
+    headers: Option[NonEmptyList[NonEmptyList[String]]] = None,
+    body:    Option[Body] = None,
+  ) {
+    def updateBody(f: Body => Body) = {
+      val updatedBody = body.fold(Body())(f)
+      copy(body = updatedBody.some)
+    }
+  }
+
+  /**
+    * Specifies rules for replace in request even if chaos target is response
+    *
+    * @param headers - Specifies the key pair used to replace the request headers or response headers
+    * @param body - Specifies request body or response body to replace the fault (Base64 encoded)
+    */
   final case class Replace(
     headers: Option[NonEmptyMap[String, String]] = None,
     body:    Option[NonEmptyList[Byte]] = None,
@@ -205,11 +271,6 @@ object HttpChaos {
 
   final private[chaosmesh] case class ResponseReplace(
     code: Option[Int] = None,
-  )
-
-  final case class Patch(
-    headers: Option[NonEmptyList[NonEmptyList[String]]] = None,
-    body:    Option[Body] = None,
   )
 
   final private[chaosmesh] case class Body(
