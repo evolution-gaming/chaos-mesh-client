ec6fcca90d84236077e43f5787693fe62cfc9367
Andrei Mikhailov <amikhailov@evolution.com>
2022-05-19 13:29:26 +0400

    Makes custom resource covrian by experiment kind

--- domain/src/main/scala/com/evolutiongaming/chaosmesh/model/k8s/CustomResource.scala
+++ domain/src/main/scala/com/evolutiongaming/chaosmesh/model/k8s/CustomResource.scala
@@ -2,7 +2,7 @@ package com.evolutiongaming.chaosmesh.model.k8s
 
 import com.evolutiongaming.chaosmesh.model.k8s.ExperimentKind
 
-abstract class CustomResource[Spec, Kind <: ExperimentKind] {
+abstract class CustomResource[Spec, +Kind <: ExperimentKind] {
   val kind:     Kind
   val spec:     Spec
   val metadata: ResourceMetadata
